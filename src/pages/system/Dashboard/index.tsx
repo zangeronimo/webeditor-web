68ddd941013d19d0ed60d575f7b449cee0a9cd4d
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-11-02 11:22:43 -0300

    added Modal to project

--- src/pages/system/Dashboard/index.tsx
+++ src/pages/system/Dashboard/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { Modal } from '../../../components/Modal';
 import { useTitle } from '../../../hooks/title';
 
 export const Dashboard: React.FC = () => {
@@ -11,24 +10,6 @@ export const Dashboard: React.FC = () => {
     <>
       <p>Bem-vindo</p>
       <p>Área para informações úteis, gráficos, informativos, etc...</p>
-
-      <div>
-        <Modal title="aeeee1" button="Open Modal 1">
-          <p>Deseja realmente exluir?</p>
-        </Modal>
-      </div>
-      <div>
-        <Modal title="aeeee2" button="Open Modal 2" />
-      </div>
-      <div>
-        <Modal title="aeeee3" button="Open Modal 3" />
-      </div>
-      <div>
-        <Modal title="aeeee4" button="Open Modal 4" />
-      </div>
-      <div>
-        <Modal title="aeeee5" button="Open Modal 5" />
-      </div>
     </>
   );
 };
